0629fa680943e5358d7c680802d86d406de22f16
kupoyuki <kupoyuki13@iamas.ac.jp>
2014-12-07 19:17:41 +0900

    アイコンつけた
    男女。

--- cf_visualize.js
+++ cf_visualize.js
@@ -3,23 +3,25 @@
 var w = $(window).width(), //横
     h = $(window).height(); //縦
 
+var padding = 50;
+
 var svg = d3.select("body").append("svg")
     .attr("width", w)
     .attr("height", h);
 
-var padding = 50;
 
 //ダミーデータ
-var dataset = [{"name":"A","x":9.291,"y":0.828},
-{"name":"B","x":2.646,"y":9.642},
-{"name":"C","x":0.265,"y":2.085},
-{"name":"D","x":9.735,"y":6.575},
-{"name":"E","x":4.213,"y":4.293},
-{"name":"F","x":8.799,"y":0.793},
-{"name":"G","x":7.941,"y":6.323},
-{"name":"H","x":7.291,"y":5.748},
-{"name":"I","x":2.629,"y":1.828},
-{"name":"J","x":1.239,"y":0.926}];
+var dataset = 
+[{"name":"A","sex":"woman","x":9.291,"y":0.828},
+{"name":"B","sex":"man","x":2.646,"y":9.642},
+{"name":"C","sex":"woman","x":0.265,"y":2.085},
+{"name":"D","sex":"woman","x":9.735,"y":6.575},
+{"name":"E","sex":"man","x":4.213,"y":4.293},
+{"name":"F","sex":"man","x":8.799,"y":0.793},
+{"name":"G","sex":"woman","x":7.941,"y":6.323},
+{"name":"H","sex":"man","x":7.291,"y":5.748},
+{"name":"I","sex":"woman","x":2.629,"y":1.828},
+{"name":"J","sex":"woman","x":1.239,"y":0.926}];
 
 //indexの追加
 for(var i = 0 ; i < dataset.length ; ++ i){
@@ -62,6 +64,17 @@ var humans = svg.selectAll(".human")
                 .on("mouseover",mouseover)
                 .on("mouseout",mouseout);
 
+
+          humans.append("image")
+                .attr("class",sex_class)
+          		.attr("xlink:href", icon)
+				.attr("x", function(d) { return xScale(d.x) -29})
+				.attr("y", function(d) { return yScale(d.y) -11})
+				.style("fill","black")
+				.attr("width", 60)
+				.attr("height", 100);
+
+
           humans.append("text")
           		.attr("class","name")
 				.attr("dx", function(d) { return xScale(d.x) + 20})
@@ -79,7 +92,23 @@ function latest_data(d){
 	}
 }
 
-//クリックイベント
+function sex_class(d){
+	return d.sex == "woman" ? "woman" : "man";
+}
+
+function icon(d){
+	if(d.sex == "woman"){
+		return "./img/f.svg";
+	}else{
+		return "./img/m.svg";
+	}
+}
+
+
+/* ----------------------------------------- *
+ * human クリック時                            *
+ * ------------------------------------------*/
+
 function click(d){
 
 	console.log(d);
