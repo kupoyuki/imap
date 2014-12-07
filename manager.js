ce305c488b25302a9c0ab7c77f1aa48d7301e229
Yusk <ishigo12@iamas.ac.jp>
2014-12-08 02:30:37 +0900

    回答システム完成！

---
+++ manager.js
@@ -0,0 +1,302 @@
+
+/* ======================================= *
+ * JSONに変換するプログラム                *
+ * ======================================= */
+jQuery.extend({
+  stringify : function stringify(obj) {
+    var t = typeof (obj);
+    if (t != "object" || obj === null) {
+      // simple data type
+      if (t == "string") obj = '"' + obj + '"';
+      return String(obj);
+    } else {
+      // recurse array or object
+      var n, v, json = [], arr = (obj && obj.constructor == Array);
+
+      for (n in obj) {
+        v = obj[n];
+        t = typeof(v);
+        if (obj.hasOwnProperty(n)) {
+          if      (t == "string")               v = '"' + v + '"';
+          else if (t == "object" && v !== null) v = jQuery.stringify(v);
+          json.push((arr ? "" : '"' + n + '":') + String(v));
+        }
+      }
+      return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+    }
+  }
+}); 
+
+function delayCall(callback, scope, time, args)
+{
+    if (typeof scope === "undefined" || scope === null) {
+        scope = this;
+    }
+  
+    if (!Array.isArray(args)) {
+        if (typeof args !== "undefined") {
+            args = [args];
+        } else {
+            args = [];
+        }
+    }
+ 
+    return setTimeout(function () { callback.apply(scope, args); },time);
+}
+
+function QuestionManager(data, q_data)
+{
+	this._data = data;												// 出力データ
+	this._q_data = q_data;											// 質問データ
+
+	this._all_question_num = q_data['all_question_num'];			// 全質問数
+	this._common_question_num = q_data['common_question_num'];		// 共通の質問数
+
+	// 共通の質問数のほうが全質問数よりも多かったらエラー
+	if (this._common_question_num > this._all_question_num)
+	{
+		alert('error!');
+	}
+
+	this._timeout_msec = q_data['timeout_msec'];					// タイムアウト時間（ミリ秒）
+
+	this._cur_question_count = 1;									// 現在の回答数
+	this._cur_word = '';
+	this._timer = null;												// タイムアウトタイマ
+	this._start_time = 0;
+}
+
+QuestionManager.prototype.startQuestion = function()
+{
+	var SELF = this;
+
+	// データ初期化
+	this._data.question = [];
+	this._cur_question_count = 1;
+	clearTimeout(this._timer);
+
+	this._start_time = $.now();
+
+	this.changeQuestion(this.nextWord());
+
+
+	// 興味あるをクリックした時の処理
+	$('#yes').click(function(e)
+	{
+		clearTimeout(SELF._timer);
+		var time = $.now() - SELF._start_time;
+
+		SELF._data.question.push({
+		    time    : time
+		  , word    : SELF._cur_word
+		  , answer  : 1
+		});
+
+		SELF._cur_question_count++;
+		if (SELF._cur_question_count > SELF._all_question_num)
+		{
+			SELF.finishQuestion();
+			return;
+		}
+
+		SELF.changeQuestion(SELF.nextWord());
+		
+
+		// 共通問題の間はタイムアウトさせない
+		if (SELF._common_question_num < SELF._cur_question_count)
+		{
+			this._timer = delayCall(SELF.passQuestion, SELF, SELF._timeout_msec);
+		}
+		SELF._start_time = $.now();
+	});
+
+
+	// 興味ないをクリックした時の処理
+	$('#no').click(function(e)
+	{
+		clearTimeout(SELF._timer);
+		var time = $.now() - SELF._start_time;
+
+		SELF._data.question.push({
+		    time    : time
+		  , word    : SELF._cur_word
+		  , answer  : -1
+		});
+
+		SELF._cur_question_count++;
+
+		SELF.changeQuestion(SELF.nextWord());
+		if (SELF._cur_question_count > SELF._all_question_num)
+		{
+			SELF.finishQuestion();
+			return;
+		}
+
+		// 共通問題の間はタイムアウトさせない
+		if (SELF._common_question_num < SELF._cur_question_count)
+		{
+			this._timer = delayCall(SELF.passQuestion, SELF, SELF._timeout_msec);
+		}
+		SELF._start_time = $.now();
+	});
+
+
+	// パスをクリックした時の処理
+	$('#pass').click(function(e)
+	{
+		SELF.passQuestion();
+	});
+
+
+	// キーバインド
+	$("body").keydown(function(e)
+	{
+		if(e.keyCode == 39)
+		{
+			//→
+			$('#no').click();
+		}
+
+		if(e.keyCode == 37 )
+		{
+			//←
+			$('#yes').click();
+		}
+
+		// 共通問題中はパスさせない
+		if (SELF._common_question_num < SELF._cur_question_count)
+		{
+			if(e.keyCode == 40 )
+			{
+				//↓
+				$('#pass').click();
+			}
+		}
+  });
+}
+
+QuestionManager.prototype.nextWord = function()
+{
+    // 共通単語から読み込み
+    if (this._common_question_num >= this._cur_question_count)
+    {
+    	return this._q_data['common_words'][this._cur_question_count-1];
+    }
+    // 分類単語から読み込み
+    else
+    {
+    	var categories = new Array();
+    	$.each(this._q_data['category_words'], function(index, val)
+    	{
+    		categories.push(index);
+    	});
+    	
+    	var category_index = Math.floor(Math.random() * categories.length);
+    	var category_words = this._q_data['category_words'][categories[category_index]];
+    	var word_num = category_words.length;
+
+    	var word_index = Math.floor(Math.random() * word_num);
+    	var word = category_words[word_index];
+
+    	// 重複させないぜ！
+    	while(true)
+    	{
+    		var is_duplicate = false;
+    		for (var i = 0; i < this._data.question.length; i++)
+    		{
+    			if (this._data.question[i]['word'] === word)
+    			{
+    				is_duplicate = true;
+    			}
+    		}
+
+    		if (is_duplicate)
+    		{
+		    	word_index = Math.floor(Math.random() * word_num);
+		    	word = category_words[word_index];
+    		}
+    		else
+			{
+				break;
+			}
+    	}
+    	return word;
+    }
+}
+
+QuestionManager.prototype.changeQuestion = function(q_word)
+{
+	$('.contents').hide();
+	$('#answer').hide();
+	$('#pagenum').hide();
+
+	// 共通問題はパスさせないぞ！
+	if (this._common_question_num >= this._cur_question_count)
+	{
+		$('#pass').hide();
+	}
+	else
+	{
+		$('#pass').show();
+	}
+
+	this._cur_word = q_word;
+
+	$('#txt')
+	.children('p.q_word')
+	.remove();
+
+	$('<p></p>')
+	.addClass('q_word')
+	.html(q_word)
+	.appendTo('#txt');
+
+	$('#pagenum').html(this._cur_question_count + '/' + this._all_question_num);
+
+	setTimeout(function()
+	{
+		$('.contents').fadeIn(60);
+		$('#answer').fadeIn(60);
+		$('#pagenum').fadeIn(60);
+	}, 500);
+}
+
+QuestionManager.prototype.passQuestion = function()
+{
+    clearTimeout(this._timer);
+
+    // パスは何も記録しないで次の質問へ
+	this.changeQuestion(this.nextWord());
+
+	if (this._common_question_num < this._cur_question_count)
+	{
+		this._timer = delayCall(this.passQuestion, this, this._timeout_msec);
+	}
+	this._start_time = $.now();
+}
+
+QuestionManager.prototype.finishQuestion = function()
+{
+	$('div.contents').hide();
+	$('#button')     .hide();
+	$('#pagenum')    .hide();
+	$('#result')     .show();
+
+	$('#result').html('送信中……');
+	$.ajax({
+		url        : 'filewrite.php'
+		, type     : 'POST'
+		, async    : false
+		, dataType : 'text'
+		, data     : {
+		  data : $.stringify(this._data)
+		}
+		, success : function(data)
+		{
+			setTimeout(function()
+			{
+			  window.location = "thx.html";
+			}, 100);
+		}
+	});
+}
